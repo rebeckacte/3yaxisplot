1d8f9252997dcc7eed1299907018c9b9ccfbe861
rebeckacte <102979819+rebeckacte@users.noreply.github.com>
2022-07-28 19:33:00 -0400

    Add files via upload
    having issues with miles/soc axis stacking over eachother, not sure how to reposition

---
+++ bingbonger.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#create DataFrame
+df1 = pd.DataFrame({'bus2001': [130],'bus2003': [130],'bus2001soc':[40],'bus2003soc':[20],'bus2001miles':[90],'bus2003miles':[99]})
+
+#add first bar to plot
+ax = df1.plot(kind="bar")
+
+#add x-axis label
+ax.set_xlabel('1', fontsize=14)
+
+#add y-axis label
+ax.set_ylabel('kwh',fontsize=16)
+
+# #define second y-axis that shares x-axis with current plot
+ax2 = ax.twinx()
+
+ax2.plot(ax.get_xticks(),
+    df1[['bus2001soc','bus2003soc']].values)
+
+# #add second line to plot
+#ax2 = df2.plot(kind="bar")
+
+# #add second y-axis label
+ax2.set_ylabel('soc', fontsize=16)
+
+
+ax3 = ax.twinx()
+ax3.plot(ax.get_xticks(),
+    df1[['bus2001miles','bus2003miles']].values)
+
+ax3.set_ylabel('miles', fontsize=16,)
+
+
+
+plt.show()
