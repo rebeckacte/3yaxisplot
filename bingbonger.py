2984f490662f77ca5535292f7c4444bf1a442563
rebeckacte <102979819+rebeckacte@users.noreply.github.com>
2022-07-28 19:34:51 -0400

    Update bingbonger.py

--- bingbonger.py
+++ bingbonger.py
@@ -7,26 +7,24 @@ df1 = pd.DataFrame({'bus2001': [130],'bus2003': [130],'bus2001soc':[40],'bus2003
 #add first bar to plot
 ax = df1.plot(kind="bar")
 
-#add x-axis label
+#add axis labels
 ax.set_xlabel('1', fontsize=14)
-
-#add y-axis label
 ax.set_ylabel('kwh',fontsize=16)
 
-# #define second y-axis that shares x-axis with current plot
+# #define second y-axis 
 ax2 = ax.twinx()
-
 ax2.plot(ax.get_xticks(),
     df1[['bus2001soc','bus2003soc']].values)
 
 # #add second y-axis label
 ax2.set_ylabel('soc', fontsize=16)
 
-
+#define third y-axis 
 ax3 = ax.twinx()
 ax3.plot(ax.get_xticks(),
     df1[['bus2001miles','bus2003miles']].values)
 
+# #add third y-axis label
 ax3.set_ylabel('miles', fontsize=16,)
 
 
