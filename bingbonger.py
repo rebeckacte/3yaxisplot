c7bca6ea3489e1d6a693492512e8321984132010
rebeckacte <102979819+rebeckacte@users.noreply.github.com>
2022-07-28 20:08:13 -0400

    Update bingbonger.py
    no more stacked axes

--- bingbonger.py
+++ bingbonger.py
@@ -1,8 +1,9 @@
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
 #create DataFrame
-df1 = pd.DataFrame({'bus2001': [130],'bus2003': [130],'bus2001soc':[40],'bus2003soc':[20],'bus2001miles':[90],'bus2003miles':[99]})
+df1 = pd.DataFrame({'bus2001': [250],'bus2003': [214],'bus2001soc':[82],'bus2003soc':[64],'bus2001miles':[93],'bus2003miles':[90]})
 
 #add first bar to plot
 ax = df1.plot(kind="bar")
@@ -26,7 +27,6 @@ ax3.plot(ax.get_xticks(),
 
 # #add third y-axis label
 ax3.set_ylabel('miles', fontsize=16,)
-
-
+ax3.spines['right'].set_position(('outward',60))
 
 plt.show()
